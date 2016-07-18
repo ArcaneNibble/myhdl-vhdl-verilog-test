4f6a98449dce1aa90c68c82cda11533c51a46c9e
Robert Ou <rqou@robertou.com>
2016-07-17 21:14:13 -0700

    Added hex printing function for SH2

--- sh2code.s
+++ sh2code.s
@@ -12,6 +12,9 @@ start:
     mov #0, r0
     mov.l r0, @r1
 
+    bsr print_word
+     nop
+
     /* Print first hello */
     mov.l sh2_done_msg_addr, r1
     bsr print
@@ -33,6 +36,61 @@ print_exit:
     rts
      nop
 
+/* r1 = val */
+print_word:
+    mov.l hexlut_addr, r2
+    mov.l debug_addr, r3
+
+    swap.w r1, r0
+    swap.b r0, r0
+    shlr2 r0
+    shlr2 r0
+    and #0x0f, r0
+    mov.b @(r0, r2), r4
+    mov.b r4, @r3
+
+    swap.w r1, r0
+    swap.b r0, r0
+    and #0x0f, r0
+    mov.b @(r0, r2), r4
+    mov.b r4, @r3
+
+    swap.w r1, r0
+    shlr2 r0
+    shlr2 r0
+    and #0x0f, r0
+    mov.b @(r0, r2), r4
+    mov.b r4, @r3
+
+    swap.w r1, r0
+    and #0x0f, r0
+    mov.b @(r0, r2), r4
+    mov.b r4, @r3
+
+    swap.b r1, r0
+    shlr2 r0
+    shlr2 r0
+    and #0x0f, r0
+    mov.b @(r0, r2), r4
+    mov.b r4, @r3
+
+    swap.b r1, r0
+    and #0x0f, r0
+    mov.b @(r0, r2), r4
+    mov.b r4, @r3
+
+    mov r1, r0
+    shlr2 r0
+    shlr2 r0
+    and #0x0f, r0
+    mov.b @(r0, r2), r4
+    mov.b r4, @r3
+
+    mov r1, r0
+    and #0x0f, r0
+    mov.b @(r0, r2), r4
+    rts
+     mov.b r4, @r3
 
 .align 4
 debug_addr:
@@ -40,6 +98,11 @@ debug_addr:
 avr_rst_addr:
     .long 0xaaaa0004
 
+.align 4
+hexlut_addr:
+    .long hexlut
+hexlut:
+    .ascii "0123456789ABCDEF"
 
 .align 4
 sh2_boot_msg_addr:
